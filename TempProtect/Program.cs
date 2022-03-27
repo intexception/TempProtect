3b0529e0b6965de31936f8169cbe7355597571e6
intexception <quantuniumgd@gmail.com>
2022-03-27 03:07:01 +0200

    Better renamer, this was too short for a tutorial so I'm leaving a commit for this.

--- TempProtect/Program.cs
+++ TempProtect/Program.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 using dnlib.DotNet;
 using TempProtect.Transform;
+using TempProtect.Transform.Impl;
 
 namespace TempProtect
 {
     internal class Program
     {
-        private static TransformerManager managerInstance ;
+	    private static TransformerManager managerInstance ;
+
         static void Main(string[] args)
         {
-            ModuleDefMD baseModule;
+	        
+	        ModuleDefMD baseModule;
 
             Console.WriteLine("Temp protect");
             Console.Write("Path to the file you want to obfuscate: ");
--- TempProtect/Transform/Impl/RenamerTransformer.cs
+++ TempProtect/Transform/Impl/RenamerTransformer.cs
@@ -14,15 +14,85 @@ namespace TempProtect.Transform.Impl
             this.Module = module;
         }
 
+        string GetRandomNumber(int length)
+        {
+            const string chars = "1234567890";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[new Random().Next(s.Length)]).ToArray());
+        }
+
+        public enum RenameVariant
+        {
+            TEMPPROTECT,
+            ALPHABET
+        }
+
+        public string GetRenamed(RenameVariant variant, int index)
+        {
+	        switch (variant)
+	        {
+		        case RenameVariant.TEMPPROTECT:
+		        {
+			        return "TEMPPROTECT0" + index;
+		        }
+		        case RenameVariant.ALPHABET:
+		        {
+			        return IndexToColumn(index).ToLower();
+		        }
+                default:
+	                return "";
+	        }
+        }
+
+        private const int ColumnBase = 26;
+        private const int DigitMax = 7; 
+        private const string Digits = "abcdefghijklmnopqrstxyz";
+
+        public static string IndexToColumn(int index)
+        {
+	        if (index <= 0)
+		        throw new IndexOutOfRangeException("index must be a positive number");
+
+	        if (index <= ColumnBase)
+		        return Digits[index - 1].ToString();
+
+	        var sb = new StringBuilder().Append(' ', DigitMax);
+	        var current = index;
+	        var offset = DigitMax;
+	        while (current > 0)
+	        {
+		        sb[--offset] = Digits[--current % ColumnBase];
+		        current /= ColumnBase;
+	        }
+	        return sb.ToString(offset, DigitMax - offset);
+        }
+
         public override void Transform(ModuleDefMD module)
         {
-            int index = 0;
+            int typeIndex = 0, methodIndex = 0, fieldIndex = 0;
             foreach (var type in module.Types)
             {
-                ++index;
+                ++typeIndex;
 
                 type.Namespace = "";
-                type.Name = "TEMPPROTECT0" + index;
+                type.Name = GetRenamed(RenameVariant.ALPHABET, typeIndex);
+
+                foreach (var method in type.Methods)
+                {
+	                if (!method.IsRuntimeSpecialName && !method.DeclaringType.IsForwarder)
+	                {
+		                ++methodIndex;
+		                method.Name = GetRenamed(RenameVariant.ALPHABET, methodIndex);
+		                foreach (var param in method.Parameters)
+		                {
+			                param.Name = GetRandomNumber(5);
+		                }
+	                }
+                }
+                foreach (var field in type.Fields)
+                {
+                    ++fieldIndex;
+                    field.Name = GetRenamed(RenameVariant.ALPHABET, fieldIndex);
+                }
             }
         }
     }
